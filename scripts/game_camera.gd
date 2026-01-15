fc2be82ddf3204b258dadadec632af4f1381c0e9
StefanLock <stefanthomaslock@gmail.com>
2026-01-15 20:40:54 +0000

    Infinte level

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,5 +1,8 @@
 extends Camera2D
 
+@onready var destroyer: Area2D = $Destroyer
+@onready var destroyer_shape: CollisionShape2D = $Destroyer/CollisionShape2D
+
 var player: Player = null
 var viewport_size 
 
@@ -10,6 +13,12 @@ func _ready() -> void:
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	destroyer.position.y = viewport_size.y / 2.0
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, 200.0)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
 	
 func _process(delta: float) -> void:
 	if player:
---
+++ scripts/level_generator.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+@onready var platform_parent: Node2D = $PlatformParent
+
+var platform_scene = preload("res://scenes/platform.tscn")
+
+var start_platform_y
+var y_distance_between_platforms = 100
+var level_size = 50
+var viewport_size
+var generated_platform_count = 0
+
+var player: Player = null
+
+func setup(_player: Player):
+	if _player:
+		player = _player
+
+func _ready() -> void:
+	generated_platform_count = 0
+	viewport_size = get_viewport().size
+	start_platform_y = viewport_size.y - (y_distance_between_platforms*2)
+	generate_level(start_platform_y, true)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player:
+		var py = player.global_position.y
+		var end_of_level_pos = start_platform_y - (generated_platform_count * y_distance_between_platforms)
+		var threshold = end_of_level_pos + (y_distance_between_platforms * 5)
+		if py <= threshold:
+			generate_level(end_of_level_pos, false)
+
+func generate_level(start_y: float, generate_ground: bool):
+	var platform_width = 155
+	
+	if generate_ground == true:
+		# Generate ground
+		var platform_height = 37.5
+		var ground_platform_count = (viewport_size.x / platform_width) + 1
+		
+		for i in range(ground_platform_count):
+			var ground_location = Vector2(i * platform_width, viewport_size.y - platform_height)
+			create_platform(ground_location)
+	
+	# Generate platforms	
+	for i in range(level_size):
+		var max_x_position = viewport_size.x - platform_width
+		var random_x = randf_range(0.0, max_x_position)
+		
+		var location: Vector2
+		location.x = random_x
+		location.y = start_y - (y_distance_between_platforms * i)
+
+		create_platform(location)
+		generated_platform_count += 1
+
+func create_platform(location: Vector2):
+	var platform = platform_scene.instantiate()
+	platform.global_position = location
+	platform_parent.add_child(platform)
+	return platform
