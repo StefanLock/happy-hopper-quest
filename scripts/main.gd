ed8850fe3e0762ac4f843fbebdd6158fefd1ceee
StefanLock <stefanthomaslock@gmail.com>
2026-03-05 14:04:40 +0000

    fixed bug wheregame was instantiead with title screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,14 @@
 extends Node
 @onready var screens: CanvasLayer = $Screens
-@onready var game: Node2D = $Game
+#@onready var game: Node2D = $Game
+
+const game = preload("uid://cikyu1m7q3dwx")
 
 func _ready() -> void:
 	screens.start_game.connect(_on_screens_start_game)
 
 func _on_screens_start_game():
-	game.new_game()
+	var game_instance = game.instantiate()
+	add_child(game_instance)
+	
+	game_instance.new_game()
